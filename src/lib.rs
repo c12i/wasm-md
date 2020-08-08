0d92dcdc76023de6ad04294ac71716245d923e93
Collins Muriuki <murerwacollins@gmail.com>
2020-08-08 15:07:41 +0300

    Fix webpack config

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-// #![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+#[allow(dead_code)]
 mod parser;
 
 use wasm_bindgen::prelude::*;
-use cfg_if::cfg_if;
 use macroz::tostr;
 
 #[wasm_bindgen]
 /// Push string from JavaScript layer to Rust layer
 pub fn parse(input: &str) -> String {
-    let result = parser::parse((tostr!(input)));
+    let result = parser::parse(tostr!(input));
     result
 }
