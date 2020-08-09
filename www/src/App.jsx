a59d3b034d49cb047a8394538bc1cc6d356db470
Collins Muriuki <murerwacollins@gmail.com>
2020-08-09 15:15:54 +0300

    2020-08-09_15-15

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import { parse } from "wasm_md";
 
 import './App.scss';
 
-const App = () => (
+const App = () => {
+  const [text, setText] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleChange = (e) => {
+    setText({searchField: e.target.value});
+  }
+
+  const handleClick = (text) => {
+    setOutput(parse(text))
+  }
+
+  return (
   <div className='app'>
-    <div className='card'>
-      <h1 className='title'>Alles gut! 👍🏾</h1>
-    </div>
+    <textarea id="markdown" onChange={handleChange} value={text}></textarea>
+    <button id="parse" onClick={(text) => handleClick(text)}>Parse the Text</button>
+    <div id="output">{output}</div>
   </div>
-);
+  )
+};
 
 export default App;
