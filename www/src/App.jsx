a21ac8a530545b1f401e68459b0f75868554d988
Collins Muriuki <murerwacollins@gmail.com>
2020-08-14 14:52:52 +0300

    Add simple clear button

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -10,6 +10,46 @@ const Title = styled.h1`
   font-family: sans-serif;
 `;
 
+const Footer = styled.footer`
+  text-align: center;
+  margin-top: 3rem;
+  font-family: sans-serif;
+  font-size: 14px;
+`;
+
+const ClearButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2rem;
+
+  .btn {
+  align-items: center;
+  font-family: 'Open Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  display: inline-block;
+  padding: 6px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 1.428571429;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  cursor: pointer;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  outline:0;
+
+&:hover,
+&:focus {
+  text-decoration: none;
+  outline:0;
+}
+
+  }
+`;
+
 const App = () => {
   const [text, setText] = useState("");
   const [output, setOutput] = useState("");
@@ -19,21 +59,31 @@ const App = () => {
     wasm.then((wasm) => {
       setOutput(wasm.parse(text.toString()));
     });
-  };
+  }
+
+  const clearScreen = () => {
+    setText("");
+    setOutput("")
+  }
 
   return (
     <>
       <Title>Wasm Markdown Parser</Title>
+      <ClearButton onClick={clearScreen}>
+        <button className="btn">Clear</button>
+      </ClearButton>
       <div className="app">
         <textarea
           className="input"
-          onChange={handleChange}
+          onChange={(e) => handleChange(e)}
           value={text}
           placeholder="Write some markdown"
         ></textarea>
-        <br />
         <div className="output" dangerouslySetInnerHTML={{ __html: output }} />
       </div>
+      <Footer>
+        <a href="https://github.com/collinsmuriuki/wasm-md">Github</a>
+      </Footer>
     </>
   );
 };
