ab654c3def4c627d899d024c8128be2fd2120fa1
Collins Muriuki <murerwacollins@gmail.com>
2020-08-14 14:00:42 +0300

    Greatly improve styling

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 const wasm = import("wasm_md");
 
-import './App.scss';
+import "./App.scss";
 
-const App = ({ parse }) => {
+const Title = styled.h1`
+  font-size: 40px;
+  text-align: center;
+  font-family: sans-serif;
+`;
+
+const App = () => {
   const [text, setText] = useState("");
   const [output, setOutput] = useState("");
 
   const handleChange = (e) => {
     setText(e.target.value);
-    wasm.then(wasm => {
-      setOutput(wasm.parse(text.toString()))
-    })
-  }
+    wasm.then((wasm) => {
+      setOutput(wasm.parse(text.toString()));
+    });
+  };
 
   return (
-  <div className='app'>
-    <h1>Wasm Markdown Preview</h1>
-    <textarea className="input" onChange={handleChange} value={text} placeholder="Input"></textarea>
-    <div className="output" dangerouslySetInnerHTML={{ __html: output }} />
-  </div>
-  )
+    <>
+      <Title>Wasm Markdown Parser</Title>
+      <div className="app">
+        <textarea
+          className="input"
+          onChange={handleChange}
+          value={text}
+          placeholder="Write some markdown"
+        ></textarea>
+        <div className="output" dangerouslySetInnerHTML={{ __html: output }} />
+      </div>
+    </>
+  );
 };
 
 export default App;
